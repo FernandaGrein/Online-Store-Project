4e510b336274e917ec3b2876c0d2aefb0cfd6905
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-08-03 18:04:09 -0300

    configura rotas

--- online-store-project/src/App.js
+++ online-store-project/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './Comṕonents/Home';
+import ShoppingCart from './Comṕonents/ShoppinhgCart';
+import ProductDetails from './Comṕonents/ProductDetails';
+import Checkout from './Comṕonents/Checkout';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route
+              path="/product/details/:id"
+              component={ ProductDetails }
+            />
+            <Route
+              path="/checkout"
+              component={ Checkout }
+            />
+            <Route
+              path="/shoppingcart"
+              component={ ShoppingCart }
+            />
+            <Route
+              exact
+              path="/"
+              component={ Home }
+            />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
 export default App;
